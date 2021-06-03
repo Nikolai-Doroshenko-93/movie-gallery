e75c95c8e0e9a4ca4165438c30ebd8fda796674a
Nikolai Doroshenko <nikolai_doroshenko@bk.ru>
2021-06-03 13:15:30 +0300

    add: movie page

--- src/js/showMovies.js
+++ src/js/showMovies.js
@@ -1,26 +1,28 @@
 import '../scss/app.scss';
 export const moviesEl = document.querySelector('.movies__container');
 
+function formatDate(date) {
+    if(date) {
+        var monthNames = [
+            "January", "February", "March",
+            "April", "May", "June", "July",
+            "August", "September", "October",
+            "November", "December"
+          ];
+        
+          var day = date.slice(8);
+          var monthIndex = parseInt(date.slice(6,8));
+          var year = date.slice(0,4);
+        
+          return day + ' ' + monthNames[monthIndex] + ' ' + year;
+    }
+  }
+
 export function showMovies (data) {   
     data.forEach((movie) => {
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
-        function formatDate(date) {
-            if(date) {
-                var monthNames = [
-                    "January", "February", "March",
-                    "April", "May", "June", "July",
-                    "August", "September", "October",
-                    "November", "December"
-                  ];
-                
-                  var day = date.slice(8);
-                  var monthIndex = parseInt(date.slice(6,8));
-                  var year = date.slice(0,4);
-                
-                  return day + ' ' + monthNames[monthIndex] + ' ' + year;
-            }
-          }
+        
         movieEl.innerHTML = `
 
         <div class="movie__cover">
@@ -28,11 +30,15 @@ export function showMovies (data) {
             src="${'https://image.tmdb.org/t/p/w500' + movie.backdrop_path}"
             class="movie__cover__img">
             </img>
-            <div class="movie__cover-blackout"></div>
+            <div class="movie__cover-blackout">    
+            </div>
+            <div class="movie__cover-blackout__movie-id">${movie.id}</div>
             <div class="movie__info__popularity-and-release-date">
+            
                 <div class="movie__info__popularity">
                     <div class="movie__info__popularity__icon"></div>
                     <p>${Math.round(movie.vote_count)}</p>
+                    
                 </div>
                 <div class="movie__info__release-date">Release date: <br> ${formatDate(movie.release_date)}</div>
             </div>
@@ -42,6 +48,9 @@ export function showMovies (data) {
         `;
         moviesEl.appendChild(movieEl);
     });
+
+    //DELETE MOVIE
+
     function delMovie () {
         let delBtnArr = document.querySelectorAll('.movie__delete-btn'); 
         for(let i = 0; i<delBtnArr.length; i++) {
@@ -86,6 +95,9 @@ export function showMovies (data) {
         }
     }
     delMovie ()
+
+    //FOR ADMIN
+
     function forAdmin() {
         let delBtnArr = document.querySelectorAll('.movie__delete-btn'); 
         let isAdmin = sessionStorage.getItem('isAdmin');
@@ -111,4 +123,94 @@ export function showMovies (data) {
         }
     } 
     forAdmin();
+
+
+    //MOVIE PAGE
+
+    function moviePage () {
+        
+        function adult(element) {
+            let adultValue;
+            if (element == true) {
+                adultValue = 'Yes'
+            } else {
+                adultValue = ' Not' 
+            }
+            return adultValue;
+        }
+
+        let movieCover = document.querySelectorAll('.movie__cover');
+        movieCover.forEach(element => {
+            element.addEventListener('click', () => {
+                let currentDivArr = element.children;
+                let currentId = parseInt(currentDivArr[2].innerHTML);
+                let currentMovie = data.find(movie => movie.id === currentId);
+                let pageMovie = document.createElement('div');
+
+                pageMovie.classList.add('modal-window-for-movie-page');
+                pageMovie.innerHTML = `
+                <div class="modal-window-for-movie-page">
+                    <div class="modal-window-for-movie-page__back-btn">
+                        Back
+                    </div>
+                    <div class="modal-window-for-movie-page__box-img">
+                        <img 
+                            src="${'https://image.tmdb.org/t/p/w500' + currentMovie.backdrop_path}"
+                            class="modal-window-for-movie-page__box-img__img">
+                        </img>
+                    </div>
+                    <div class="modal-window-for-movie-page__box-info">
+                        <div class="modal-window-for-movie-page__box-info__title">
+                            <strong>${currentMovie.title}</strong>
+                        </div>
+                        <div class="modal-window-for-movie-page__release-date">
+                            <strong>Release date:</strong>
+                            ${formatDate(currentMovie.release_date)}
+                        </div>
+                        <div class="modal-window-for-movie-page__box-info__rating">
+                            <strong>Rating:</strong> 
+                            ${currentMovie.vote_average}
+                        </div>
+                        <div class="modal-window-for-movie-page__box-info__likes">
+                            <strong>Likes:</strong> 
+                            ${currentMovie.vote_count}
+                        </div>
+                        <div class="modal-window-for-movie-page__box-info__adult">
+                            <strong>Adult:</strong> 
+                            ${adult(currentMovie.adult)}
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-window-for-movie-page__overview">
+                    <p>${currentMovie.overview}</p>
+                </div>
+                `
+                let scrollValue = scrollY;
+
+                let body = document.querySelector('.body');
+                body.scrollIntoView();
+                moviesEl.appendChild(pageMovie);
+                moviesEl.classList.add('movies__container__movie-page-is-active');
+
+                let paginationBox = document.querySelector('.pagination');
+                paginationBox.style.display = 'none';
+
+                let selectOfHeader = document.querySelector('.header__select');
+                selectOfHeader.style.display = 'none';
+                function moviePageBackBtn () {
+                    let backBtn = pageMovie.querySelector('.modal-window-for-movie-page__back-btn');
+                    backBtn.addEventListener('click', () => {
+                        paginationBox.style.display = 'flex';
+                        selectOfHeader.style.display = 'block';
+                        pageMovie.remove();
+                        moviesEl.classList.remove('movies__container__movie-page-is-active');  
+                        scroll(0, scrollValue);
+                    })
+                }
+                moviePageBackBtn ();
+            })
+        })
+
+    }
+    moviePage ();
 }
